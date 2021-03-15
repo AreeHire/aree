d6ce49bbb582ba8abbb314fb39d4e2d5dc4d729e
Alberto Romero <lguitarras0594@gmail.com>
2021-03-14 21:49:33 -0600

    Add Create Button

--- pages/exams.vue
+++ pages/exams.vue
@@ -1,27 +1,32 @@
 <template>
   <div class="h-max mx-6">
-    <Title text="Exams" />
+    <div class="flex justify-between items-center">
+      <Title text="Exams" />
+      <button class="bg-gray-400 text-lg p-2 rounded-md p-1">
+        + Create
+      </button>
+    </div>
     <div class="flex flex-row flex-wrap w-full h-max justify-between">
       <exam-card
-        class="w-2/3 mb-4"
+        class="w-1/2 mb-4"
         name="Software Engineer"
         :skills="['Javascript', 'Python']"
         :applications="15"
       />
       <exam-card
-        class="w-2/3 mb-4"
+        class="w-1/2 mb-4"
         name="Founding Engineer"
         :skills="['WASM', 'Rust']"
         :applications="142"
       />
       <exam-card
-        class="w-2/3 mb-4"
+        class="w-1/2 mb-4"
         name="Founding Engineer"
         :skills="['WASM', 'Rust']"
         :applications="142"
       />
       <exam-card
-        class="w-2/3 mb-4"
+        class="w-1/2 mb-4"
         name="Founding Engineer"
         :skills="['WASM', 'Rust']"
         :applications="142"
