804e05487bf4cba388e511c952c22b61670b6be5
Alberto Romero <lguitarras0594@gmail.com>
2021-03-31 16:55:42 -0600

    Refactor ExamWelcome component to include better props

--- components/ExamWelcome.vue
+++ components/ExamWelcome.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="text-center" @click="$emit('click')">
-    <p class="mb-4">We welcome you to the challenge for the {{ exam }} exam!</p>
-    <p class="mb-4">
-      It will consist on
-      <span class="text-cyan-700">{{ numberOfQuestions }} questions</span>. The
-      results will be submitted to our platform and in case you did good, we
+    <p class="mb-1">
+      We welcome you to the challenge for the {{ exam }} exam! It will consist
+      on
+      <span class="text-cyan-700">{{ numberOfQuestions }} questions</span> you
+      will have to solve in {{ minutes }} minutes.
+    </p>
+    <p class="mb-2">
+      The results will be submitted to our platform and in case you did good, we
       will get in touch with the following steps.
     </p>
-    <p class="mb-4">Good luck!</p>
-    <Button class="w-1/2" @click="$emit('click')">
+    <p class="mb-1">Good luck!</p>
+    <Button class="w-1/4" @click="$emit('click')">
       Start
     </Button>
   </div>
@@ -16,6 +19,19 @@
 
 <script>
 export default {
-  props: ["exam", "numberOfQuestions"]
+  props: {
+    exam: {
+      type: String,
+      required: true
+    },
+    minutes: {
+      type: Number,
+      required: true
+    },
+    numberOfQuestions: {
+      type: Number,
+      required: true
+    }
+  }
 };
 </script>
